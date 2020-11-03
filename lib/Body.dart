1f09e5307a691598e9807000f961aed0ddf84501
Busher Alani <busher.2004a@gmail.com>
2020-11-03 23:58:01 +0300

    I done AppBar Widget, I wrote "Hello", "Matth Nunez" .

--- lib/Body.dart
+++ lib/Body.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:new_app2/Const.dart';
 
 class BodyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Column(
       children: <Widget>[
+        Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            height: size.height * 0.1,
+            width: size.height * 0.5,
+            color: kBackgroundColor,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Hello",
+                    style: Theme.of(context).textTheme.headline1.copyWith(
+                          color: Colors.white,
+                          fontFamily: "VarelaRound-Regular",
+                          fontSize: 23,
+                        )),
+                Text("Matth Nunez",
+                    style: Theme.of(context).textTheme.headline2.copyWith(
+                          color: Colors.white,
+                          fontFamily: "VarelaRound-Regular",
+                          fontSize: 30,
+                        )),
+              ],
+            )),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Body.dart';
+import 'package:new_app2/Body.dart';
 import 'Const.dart';
 
 void main() {
@@ -10,32 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        scaffoldBackgroundColor: kBackgroundColor,
-        primarySwatch: kPrimaryColor,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          // menu Icon
-          leading: Icon(Icons.apps),
-          actions: <Widget>[
-            // Avatar Icon in top left
-            Align(
-              alignment: Alignment.centerLeft,
-              child: CircleAvatar(
-                backgroundColor: Colors.black,
-                radius: 28,
-                backgroundImage: NetworkImage(
-                    "https://gigantic.store/wp-content/uploads/2019/04/09.jpg"),
-              ),
-            ),
-          ],
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: kBackgroundColor,
+          textTheme:
+              Theme.of(context).textTheme.apply(bodyColor: kPrimaryColor),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        body: BodyPage(),
-      ),
-    );
+        home: Scaffold(
+          backgroundColor: Colors.teal[900],
+          appBar: AppBar(
+            elevation: 0,
+            leading: Icon(Icons.apps),
+            actions: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 28,
+                  backgroundImage: NetworkImage(
+                      "https://gigantic.store/wp-content/uploads/2019/04/09.jpg"),
+                ),
+              ),
+            ],
+          ),
+          body:BodyPage() , 
+        ));
   }
 }
