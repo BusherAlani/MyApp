814b6fbf7a002db54b5c8c03260da8b05d40cbfe
Busher Alani <busher.2004a@gmail.com>
2020-11-04 00:24:30 +0300

    I make Container White for make List inside .

--- lib/Body.dart
+++ lib/Body.dart
@@ -8,27 +8,50 @@ class BodyPage extends StatelessWidget {
     return Column(
       children: <Widget>[
         Container(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            height: size.height * 0.1,
-            width: size.height * 0.5,
-            color: kBackgroundColor,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Hello",
-                    style: Theme.of(context).textTheme.headline1.copyWith(
-                          color: Colors.white,
-                          fontFamily: "VarelaRound-Regular",
-                          fontSize: 23,
-                        )),
-                Text("Matth Nunez",
-                    style: Theme.of(context).textTheme.headline2.copyWith(
-                          color: Colors.white,
-                          fontFamily: "VarelaRound-Regular",
-                          fontSize: 30,
-                        )),
-              ],
-            )),
+          padding: EdgeInsets.only(left: 20, right: 20),
+          height: size.height * 0.1,
+          width: size.height * 0.5,
+          color: kBackgroundColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Hello",
+                  style: Theme.of(context).textTheme.headline1.copyWith(
+                        color: Colors.white,
+                        fontFamily: "VarelaRound-Regular",
+                        fontSize: 23,
+                      )),
+              Text("Matthew Nunez",
+                  style: Theme.of(context).textTheme.headline2.copyWith(
+                        color: Colors.white,
+                        fontFamily: "VarelaRound-Regular",
+                        fontSize: 30,
+                      )),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(
+            left: 5,
+            right: 5,
+          ),
+          width: 700,
+          height: 650,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(1.0),
+              )
+            ],
+          ),
+          child: Column(
+            children: <Widget>[
+              
+            ]
+          ),
+        ),
       ],
     );
   }
